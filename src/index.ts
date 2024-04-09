47856ecb96e3c11afda78b7df4434d523ec7e2c0
Mark Martirosian <hello@markmartirosian.com>
2024-04-09 23:59:46 +0000

    chore: eslint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // Copyright © 2020-2023 Truestamp Inc. All rights reserved.
 
 /**
